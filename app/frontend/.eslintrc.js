b02efda609f11ee23d8e52bfd66c253fe76fa45a
nic <>
2020-12-23 17:52:07 -0500

    vue.js initial commit

---
+++ app/frontend/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true
+    },
+    extends: [
+        'plugin:vue/essential',
+        '@vue/standard'
+    ],
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'vue/script-indent': ['error', 4],
+        'vue/html-indent': ['error', 4],
+        indent: ['error', 4]
+    },
+    overrides: [
+        {
+            files: [
+                '**/__tests__/*.{j,t}s?(x)',
+                '**/tests/unit/**/*.spec.{j,t}s?(x)'
+            ],
+            env: {
+                jest: true
+            }
+        }
+    ]
+}
